d82c6dd64f8908b8c0ded67c2c7debfd3ec235a3
Lucas Zani <zanin0503@gmail.com>
2025-11-04 10:33:49 -0300

    feat: add columns and add inserts in all tables

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -6,7 +6,10 @@ CREATE TABLE patient (
     id SERIAL PRIMARY KEY,
     patient_name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    cpf INT NOT NULL,
+    number_phone INT NOT NULL
 );
 
 CREATE TABLE especialty (
@@ -19,6 +22,8 @@ CREATE TABLE doctor (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    number_phone INT NOT NULL
     especialty_id INT REFERENCES especialty(id)
 );
 
@@ -26,7 +31,8 @@ CREATE TABLE schedule (
     id SERIAL PRIMARY KEY,
     patient_id INT REFERENCES patient(id),
     doctor_id INT REFERENCES doctor(id),
-    consult_date DATE NOT NULL
+    consult_date DATE NOT NULL,
+    consult_hour TIME NOT NULL
 );
 
 CREATE TABLE medicalrecord (
@@ -34,3 +40,13 @@ CREATE TABLE medicalrecord (
     patient_info TEXT,
     patient_id INT REFERENCES patient(id)
 );
+
+INSERT INTO patient(patient_name, email, password, birth_date, cpf, number_phone) VALUES('Roberto', 'roberto123@gmail.com', '123kopk', '2025-12-04', 76755347861, 1940028922);
+
+INSERT INTO especialty(especialty) VALUES('Cardiologista');
+
+INSERT INTO doctor(name, email, password, birth_date, number_phone, especialty_id) VALUES('Dr. Ana Silva', 'ana.silva@gmail.com', '312318u', '1980-05-15', 21987654321, 1);
+
+INSERT INTO schedule(patient_id, doctor_id, consult_date, consult_hour) VALUES(1, 1, '2025-12-10', '10:30:00');
+
+INSERT INTO medicalrecord(patient_info, patient_id) VALUES('Paciente possui problemas no coração', 1);
